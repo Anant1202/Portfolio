19afc7bfb4193cd691644ef0bc15d65625274c3b
Anant Singh <anant.singh2@bajajfinserv.in>
2024-03-27 17:25:10 +0530

    header navigation changes added

--- src/App.js
+++ src/App.js
@@ -13,38 +13,39 @@ import {
 function App() {
   return (
     <div className="App">
-      
-      <Header/>
+
       <Router>
-                <Routes>
-                    
-                    <Route
-                        exact
-                        path="/"
-                        element={<Homepage/>}
-                    />
-                
-                    <Route
-                        path="/about"
-                        element={<About/>}
-                    />
-                 
-                    <Route
-                        path="/workpage"
-                        element={<WorkPage/>}
-                    />
-                 
-                    <Route
-                        path="/contactpage"
-                        element={<ContactPage/>}
-                    />
- 
-                    <Route
-                        path="*"
-                        element={<Homepage/>}
-                    />
-                </Routes>
-            </Router>
+      <Header />
+
+        <Routes>
+
+          <Route
+            exact
+            path="/"
+            element={<Homepage />}
+          />
+
+          <Route
+            path="/about"
+            element={<About />}
+          />
+
+          <Route
+            path="/workpage"
+            element={<WorkPage />}
+          />
+
+          <Route
+            path="/contactpage"
+            element={<ContactPage />}
+          />
+
+          <Route
+            path="*"
+            element={<Homepage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom"
 import "../assets/css/header.css"
 import "../assets/css/workpage.css"
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <>
       <header className="header">
@@ -9,38 +11,40 @@ function Header() {
           <div className="headercontainer">
             <span className="name">ANANT VIKRAM SINGH</span>
             <div >
-              <ul className="menubar"  data-animation="to-top">
-                <li>
-                  <a href="/">
-                    <span>Home</span>
-                    <span>
-                      <i class="fa-solid fa-house" aria-hidden="true"></i>
-                    </span>
-                  </a>
+              <ul className="menubar" data-animation="to-top">
+                <li onClick={() => navigate("/")}>
+
+                  <span>Home</span>
+                  <span>
+                    <i class="fa-solid fa-house" aria-hidden="true"></i>
+                  </span>
+
+                </li>
+                <li onClick={() => navigate("/about")}>
+                  {/* <a href="/about"> */}
+                  <span>About</span>
+                  <span>
+                    <i class="fa-solid fa-address-card" aria-hidden="true"></i>
+                  </span>
+                  {/* </a> */}
                 </li>
                 <li>
-                  <a href="/about">
-                    <span>About</span>
-                    <span>
-                      <i class="fa-solid fa-address-card" aria-hidden="true"></i>
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/workpage">
+                  <li onClick={() => navigate("/workpage")}>
+                    {/* <a href="/workpage"> */}
                     <span>Work</span>
                     <span>
                       <i class="fa-solid fa-list-check" aria-hidden="true"></i>
                     </span>
-                  </a>
+                  </li>
                 </li>
                 <li>
-                  <a href="/contactpage">
+                <li onClick={() => navigate("/contactpage")}>
+                  {/* <a href="/contactpage"> */}
                     <span>Contact</span>
                     <span>
                       <i class="fa-solid fa-phone" aria-hidden="true"></i>
                     </span>
-                  </a>
+                  </li>
                 </li>
               </ul>
             </div>
