a2f31ea10342f27676bbfb7d77a9a6d0254b9ad8
Anant Singh <anant.singh2@bajajfinserv.in>
2024-03-27 14:50:08 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Homepage from './Pages/HomePage';
@@ -9,74 +8,43 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
       <Header/>
       <Router>
                 <Routes>
-                    {/* This route is for home component 
-          with exact path "/", in component props 
-          we passes the imported component*/}
+                    
                     <Route
                         exact
                         path="/"
                         element={<Homepage/>}
                     />
-                    {/* This route is for about component 
-          with exact path "/about", in component 
-          props we passes the imported component*/}
+                
                     <Route
                         path="/about"
                         element={<About/>}
                     />
-                    {/* This route is for work component 
-          with exact path "/workpage", in component 
-          props we passes the imported component*/}
+                 
                     <Route
                         path="/workpage"
                         element={<WorkPage/>}
                     />
-                    
-                    {/* This route is for contactpage component
-          with exact path "/contactpage", in 
-          component props we passes the imported component*/}
+                 
                     <Route
                         path="/contactpage"
                         element={<ContactPage/>}
                     />
  
-                    {/* If any route mismatches the upper 
-          route endpoints then, redirect triggers 
-          and redirects app to home component with to="/" */}
-                    {/* <Redirect to="/" /> */}
                     <Route
                         path="*"
                         element={<Homepage/>}
                     />
                 </Routes>
             </Router>
-      
-      {/* <Homepage/> */}
-      {/* <ContactPage/> */}
-      {/* {<WorkPage/>} */}
     </div>
   );
 }
